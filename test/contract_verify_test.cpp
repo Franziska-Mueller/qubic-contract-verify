4a6d391c657c5eb86d953cc522387a3802262cf1
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-06-02 18:21:57 +0200

    make test files available in test project

--- test/contract_verify_test.cpp
+++ test/contract_verify_test.cpp
@@ -1,10 +1,13 @@
+#include <filesystem>
+
 #include <gtest/gtest.h>
 
 #include "check_compliance.h"
+#include "test_files_config.h"
 
 
 TEST(ContractVerifyTest, ParsingWorks) {
-    std::string inputPath = "D:\\Programming\\Repos\\qubic-contract-verify\\test\\testfiles\\test_ok.h";
-    std::unique_ptr<cppast::CppCompound> ast = contractverify::parseAST(inputPath);
+    std::filesystem::path filepath = std::filesystem::path(testfiles::baseDir).append("test_ok.h");
+    std::unique_ptr<cppast::CppCompound> ast = contractverify::parseAST(filepath.string());
     EXPECT_NE(ast, nullptr);
 }
