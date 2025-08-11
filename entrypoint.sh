56e92a49df2e1d3a334b3beb8ac2ba469728645a
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-08-11 17:37:21 +0200

    add success output to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,10 @@ printf " '%s'" "$@"
 printf '\n'
 
 /contract-verify/build/src/contractverify $@
+
+if [ $? == 0 ];
+then
+    echo "success=true" >> $GITHUB_OUTPUT
+else
+    echo "success=false" >> $GITHUB_OUTPUT
+fi
