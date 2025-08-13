c24c745fef42196e576650df7853c37b3ecb01df
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-08-13 15:19:30 +0200

    split input filepaths and run tool on each file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,20 @@ printf '%d args:' "$#"
 printf " '%s'" "$@"
 printf '\n'
 
-if /contract-verify/build/src/contractverify "$@"
+# Split the provided input based on the comma delimiter
+filepaths=$(echo "$1" | tr "," "\n")
+
+for file in $filepaths
+do
+  if ! /contract-verify/build/src/contractverify "$file"
+  then
+    failed=true
+  fi
+done
+
+if [ "$failed" = true ]
 then
-    exit 0
+  exit 1
 else
-    exit 1
+  exit 0
 fi
