6f70bacba6fbfd11c0c68838b7c6cd25892103d3
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-08-13 15:33:10 +0200

    print filename before running tool

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# `$#` expands to the number of arguments and `$@` expands to the supplied `args`
-printf '%d args:' "$#"
-printf " '%s'" "$@"
-printf '\n'
-
 # Split the provided input based on the comma delimiter
 filepaths=$(echo "$1" | tr "," "\n")
 
 for file in $filepaths
 do
+  printf "Running contract verification on '%s'\n" "$file"
   if ! /contract-verify/build/src/contractverify "$file"
   then
     failed=true
