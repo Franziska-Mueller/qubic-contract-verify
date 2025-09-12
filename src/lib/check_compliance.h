dec21a44d0d71800671eeca99e3c95f3d07cc724
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-09-12 11:25:26 +0200

    add some comments

--- src/lib/check_compliance.h
+++ src/lib/check_compliance.h
@@ -18,7 +18,10 @@ namespace contractverify
 
     bool checkCompound(const cppast::CppCompound& compound, const std::string& stateStructName, AnalysisData& analysisData);
 
+    // Tries to parse an abstract syntax tree from the specified file. Returns nullptr if the file does not exist or parsing fails. 
     std::unique_ptr<cppast::CppCompound> parseAST(const std::string& filepath);
 
+    // Finds the state struct name in an AST assuming that the state struct is the first top-level struct that inherits from ContractBase.
+    // Returns an empty string if it cannot find such a struct.
     std::string findStateStructName(const cppast::CppCompound& ast);
 }
